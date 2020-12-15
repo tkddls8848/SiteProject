1644180ac8b0ec018358be3033f6c64495215ecd
PSI <tkddls8848@gmail.com>
2020-12-15 21:55:49 +0900

    make index.html

--- homepage/homepage/settings.py
+++ homepage/homepage/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '(ou0z6848a5b7_5nn6$lvfjepnt98j+#!mmfy-$&4i+(m$6e+c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- homepage/main/views.py
+++ homepage/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('임시페이지입니다.')
+    return render(request, 'main/index.html')
